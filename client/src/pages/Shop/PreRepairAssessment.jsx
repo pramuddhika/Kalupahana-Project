e503fbc30d90ea3b153fb5c6f68ce89f1170596b
Rasanga Pramuddhika <91723368+pramuddhika@users.noreply.github.com>
2024-04-25 18:55:58 +0530

    Add pre-repair assessment checklist to PreRepairAssessment.jsx

--- client/src/pages/Shop/PreRepairAssessment.jsx
+++ client/src/pages/Shop/PreRepairAssessment.jsx
@@ -5,6 +5,27 @@ const PreRepairAssessment = () => {
         <div>
         <ShopHeader pageName="Pre-repair Assessment"/>
         <div className="h-9 bg-side-nav-bg border-b-2 "/>
+        
+        <div className="flex justify-center mt-7">
+          <div className="card w-10/12 p-2">
+
+           <div className="border-2">
+            <p>Check Llist</p>
+            <div className="flex">
+              <div className="flex items-center"> 
+                <label className="flex items-center cursor-pointer ">
+                 <input type="checkbox" className="w-4 h-4 mr-2"/>
+                 <p>Tool box</p>
+                </label>
+                
+              </div>
+            </div>
+           </div>
+            
+         
+             
+          </div>
+        </div>
             
         </div>
     );

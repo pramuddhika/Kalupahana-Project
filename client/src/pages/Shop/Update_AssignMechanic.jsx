dce8fd95d62c9094c76913c15435ae5d6aa0e100
pramuddhika <pramuddhika21@gmail.com>
2024-04-28 13:19:18 +0530

    project install in new lap

--- client/src/pages/Shop/Update_AssignMechanic.jsx
+++ client/src/pages/Shop/Update_AssignMechanic.jsx
@@ -1,14 +1,8 @@
-import Update_details from "../components/Update_details";
+
 
 const Update_AssignMechanic = () => {
     return (
         <div className="flex">
-          <div className="basis-1/4">
-            <Update_details/>
-          </div>
-          <div>
-
-          </div>
             <p>content for update Assign mechanic</p>
         </div>
     );

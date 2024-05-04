8a28ec81891ecddbe052239160505f82bd26bd6c
Rasanga Pramuddhika <91723368+pramuddhika@users.noreply.github.com>
2024-05-04 23:11:43 +0530

    chore: Refactor StockAddPart and StockAddPurchases components

--- client/src/pages/Shop/StockAddPart.jsx
+++ client/src/pages/Shop/StockAddPart.jsx
@@ -3,7 +3,19 @@
 const StockAddPart = () => {
     return (
         <div>
-            <p>this is content for availble stock</p>
+            
+            <div className="flex justify-center items-center gap-6">
+              <div className="card w-5/12">
+                <p className="topic text-xl p-4">Part Details</p>
+              </div>
+              <div className="w-5/12 card mt-24  overflow-auto h-96">
+              <p className="mt-36">table</p>
+              <p className="mt-36">table</p>
+              <p className="mt-36">table</p>
+              <p className="mt-36">table</p>
+              </div>
+            </div>
+
         </div>
     );
 };
--- client/src/pages/Shop/StockAddPurchases.jsx
+++ client/src/pages/Shop/StockAddPurchases.jsx
@@ -4,27 +4,27 @@ const StockAddPurchases = () => {
     return (
         <div className="flex justify-center gap-8">
 
-          <div className="card w-5/12 p-6 mt-28">
+          <div className="card w-5/12 p-6 mt-28 h-84">
             <p className="topic text-xl mb-4">Purchases Details</p>
 
             <div className="flex justify-center fornt-inter items-center mb-4">
               <p className="basis-1/4 text-text-primary font-semibold">Part Id:</p>
-              <input type="text" required className="input basis-1/2 rounded-lg p-2"/>
+              <input type="text" required className="input basis-1/2 rounded-lg p-2 pl-4"/>
             </div>
 
             <div className="flex justify-center fornt-inter items-center mb-4">
               <p className="basis-1/4 text-text-primary font-semibold">Part Name:</p>
-              <input type="text" required className="input basis-1/2 rounded-lg p-2"/>
+              <input type="text" required className="input basis-1/2 rounded-lg p-2 pl-4"/>
             </div>
 
             <div className="flex justify-center fornt-inter items-center mb-4">
-              <p className="basis-1/4 text-text-primary font-semibold">Part Name:</p>
-              <input type="date" required className="input basis-1/2 rounded-lg p-2"/>
+              <p className="basis-1/4 text-text-primary font-semibold">Date:</p>
+              <input type="date" required className="input basis-1/2 rounded-lg p-2 pl-4"/>
             </div>
 
             <div className="flex justify-center fornt-inter">
               <p className="basis-1/4 text-text-primary font-semibold mt-3">Quantity:</p>
-              <input type="number" required className="input basis-1/2 rounded-lg p-2"/>
+              <input type="number" required className="input basis-1/2 rounded-lg p-2 pl-4"/>
             </div>
 
             <div className="flex justify-center mt-6 gap-4">
@@ -34,19 +34,23 @@ const StockAddPurchases = () => {
 
           </div>
 
-          <div className="card w-5/12 mt-28 p-6">
+          <div className="card w-5/12 mt-28 p-6 h-84">
              <p className="topic text-xl mb-4">Today Purchases</p>
 
-             <table className="border-separate border-spacing-2 border border-slate-500 w-11/12">
-              <tr>
-                <th className="border border-slate-600 ">Part ID</th>
-                <th className="border border-slate-600 ">Quantity</th>
-              </tr>
-              <tr className="text-center">
-                <td className="border border-slate-600"> hi</td>
-                <td className="border border-slate-600"> hu</td>
-              </tr>
-             </table>
+             <div className="flex justify-center overflow-auto max-h-80">
+              <table className="border-separate border-spacing-2 border border-slate-500 w-11/12">
+                <tr>
+                  <th className="border border-slate-600 ">Part ID</th>
+                  <th className="border border-slate-600 ">Quantity</th>
+                </tr>
+
+                <tr className="text-center">
+                  <td className="border border-slate-600"> hi</td>
+                  <td className="border border-slate-600"> hu</td>
+                </tr>
+    
+              </table>
+              </div>
 
           </div>
         </div>

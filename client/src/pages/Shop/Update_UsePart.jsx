a369af590f47477d9e56e8aa9e2fa5e748c330e5
Rasanga Pramuddhika <91723368+pramuddhika@users.noreply.github.com>
2024-05-09 08:10:47 +0530

    Refactor input form in Update_UsePart component

--- client/src/pages/Shop/Update_UsePart.jsx
+++ client/src/pages/Shop/Update_UsePart.jsx
@@ -6,36 +6,6 @@ const Update_UsePart = () => {
         
           <div className="flex flex-col justify-center items-center basis-2/3">
 
-            {/* <div className="card mt-5 w-11/12 p-2">
-              <p className="topic">Used Parts</p>
-            <div className=" justify-between">
-
-            <div className="flex justify-center items-center gap-16">
-              <div className="flex justify-center fornt-inter items-center mb-4">
-               <p className="basis-1/3 text-text-primary font-semibold">Part Id:</p>
-               <input type="text" required className="input basis-1/2 rounded-lg p-2 pl-4 w-48" placeholder='Add part ID'/>
-              </div>
-
-              <div className="flex justify-center fornt-inter items-center mb-4">
-               <p className="basis-1/3 text-text-primary font-semibold">Part Name:</p>
-               <input type="text" required className="input basis-1/2 rounded-lg p-2 pl-4" placeholder='Add part Name'/>
-              </div>
-            </div>
-
-            <div className="flex justify-between items-center"></div>
-                
-           
-
-            <div className="flex justify-center fornt-inter">
-              <p className="basis-1/4 text-text-primary font-semibold mt-3">Quantity:</p>
-              <input type="number" required className="input basis-1/2 rounded-lg p-2 pl-4" placeholder="Number of units"/>
-            </div>
-
-             <div className="flex items-end mb-4 mr-16">
-               <button className="btn btn-normal">Send</button>
-             </div>
-            </div>
-            </div> */}
             {/**input data form - start */}
             <div className="card w-11/12 p-2 mt-5">
              

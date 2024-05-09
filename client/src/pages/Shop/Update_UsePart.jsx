57bc93ec2ab543cb7c69b6e0adedbc10d1d26a40
Rasanga Pramuddhika <91723368+pramuddhika@users.noreply.github.com>
2024-05-09 08:10:24 +0530

    Refactor input form in Update_UsePart component

--- client/src/pages/Shop/Update_UsePart.jsx
+++ client/src/pages/Shop/Update_UsePart.jsx
@@ -36,6 +36,39 @@ const Update_UsePart = () => {
              </div>
             </div>
             </div> */}
+            {/**input data form - start */}
+            <div className="card w-11/12 p-2 mt-5">
+             
+              <p className="topic ">Used Part</p>
+
+              <div className="flex my-4 justify-between">
+                <div className="flex justify-center items-center font-inter gap-5">
+                  <p className="text-text-primary font-semibold w-24">Part ID: </p>
+                  <input type="text" className="input p-2 rounded-lg" placeholder="Search by Part ID"/>
+                </div>
+                <div className="flex justify-center items-center font-inter gap-5">
+                  <p className="text-text-primary font-semibold">Part Name: </p>
+                  <input type="text" className="input p-2 rounded-lg" placeholder="Search by Part Name"/>
+                </div>
+              </div>
+
+              <div className="flex my-4 justify-between">
+                <div className="flex justify-center items-center font-inter gap-5">
+                  <p className="text-text-primary font-semibold w-24">Quantity : </p>
+                  <input type="number" className="input p-2 rounded-lg" placeholder=""/>
+                </div>
+                <div className="flex justify-center items-center font-inter gap-5 mr-12">
+                  <button className="btn btn-warning">Clean</button>
+                  <button className="btn btn-normal">Add</button>
+                </div>
+              </div>
+
+
+                
+              
+            </div>
+            {/**input data form -end */}
+
 
             {/**table - start */}
             <div className="box-content h-80 card mt-5 w-11/12 p-2">

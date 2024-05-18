6478eb9634fadf339752c2c8c41091373214c487
Rasanga Pramuddhika <91723368+pramuddhika@users.noreply.github.com>
2024-05-18 19:48:15 +0530

    feat: Add mechanic route for adding mechanic details

--- client/src/pages/Shop/MechanicsAdd.jsx
+++ client/src/pages/Shop/MechanicsAdd.jsx
@@ -13,7 +13,6 @@ const MechanicsAdd = () => {
 
   const [openCancelModal,setOpenCancelModal] = useState(false);
   const [mechanicsList, setMechanicsList] = useState(null);
- 
   const [addMechanic,setAddMechanic] = useState({
     employeeId:"",
     employeeName:"",
@@ -23,7 +22,6 @@ const MechanicsAdd = () => {
     mainArea:"",
     subArea:""
   })
-
   const navigate = useNavigate();
 
   //fetch mechanoc specalist are to the select list
@@ -70,52 +68,49 @@ const MechanicsAdd = () => {
   //handle add mechanic data submit
   const handleAddMechanicSubmit = async (e) => {
     e.preventDefault();
-    //use input validation
     
-  const { employeeName, employeeId,contactNumber } = addMechanic;
-  const nameRegex = /^[a-zA-Z\s]*$/;
-  const idRegex = /^[A-Z]+-\d+$/;
-  const teleRegex = /^07[0-8]\d{7}$/;
-  const emptyStringRegex = /^$/;
+    //use input validation
+    const { employeeName, employeeId,contactNumber } = addMechanic;
+    const nameRegex = /^[a-zA-Z\s]*$/;
+    const idRegex = /^[A-Z]+-\d+$/;
+    const teleRegex = /^07[0-8]\d{7}$/;
+    const emptyStringRegex = /^$/;
 
-  if (!idRegex.test(employeeId)) {
-    toast.warning("Invalid employee ID!");
-    return;
-  }
-  if (!nameRegex.test(employeeName)) {
-    toast.warning("Invalid employee name!");
-    return;
-  }
-  if (emptyStringRegex.test(addMechanic.employeeName)) {
-    toast.warning("Name can't be empty!");
-    return;
-  }
-  
-  if(!teleRegex.test(contactNumber)){
-    toast.warning("Invalid contact number!");
-    return;
-  }
-  if (emptyStringRegex.test(addMechanic.livingArea)) {
-    toast.warning("Living area can't be empty!");
-    return;
-  }
-  if (emptyStringRegex.test(addMechanic.joinDate)) {
-    toast.warning("Join date can't be empty!");
-    return;
-  }
-  let joinDate = new Date(addMechanic.joinDate);
-let currentDate = new Date();
-
-if(joinDate > currentDate){
-  toast.warning("Join date can't be a future date!");
-  return;
-}
-  if (emptyStringRegex.test(addMechanic.mainArea)) {
-    toast.warning("Main specialist area can't be empty!");
-    return;
-  }
-  
-
+    if (!idRegex.test(employeeId)) {
+     toast.warning("Invalid employee ID!");
+     return;
+    }
+    if (!nameRegex.test(employeeName)) {
+     toast.warning("Invalid employee name!");
+     return;
+    }
+    if (emptyStringRegex.test(addMechanic.employeeName)) {
+     toast.warning("Name can't be empty!");
+     return;
+    }
+    if(!teleRegex.test(contactNumber)){
+     toast.warning("Invalid contact number!");
+     return;
+    }
+    if (emptyStringRegex.test(addMechanic.livingArea)) {
+     toast.warning("Living area can't be empty!");
+     return;
+    }
+    if (emptyStringRegex.test(addMechanic.joinDate)) {
+     toast.warning("Join date can't be empty!");
+     return;
+    }
+    let joinDate = new Date(addMechanic.joinDate);
+    let currentDate = new Date();
+    if(joinDate > currentDate){
+     toast.warning("Join date can't be a future date!");
+     return;
+    }
+    if (emptyStringRegex.test(addMechanic.mainArea)) {
+     toast.warning("Main specialist area can't be empty!");
+     return;
+    }
+    //handle add data
     try{
       const res = await axios.post('http://localhost:8000/api/mechanic/addmechanic', addMechanic);
       toast.success(res.data);
@@ -127,104 +122,102 @@ if(joinDate > currentDate){
     }
   }
 
-    return (
-        <div>
-            <ShopHeader pageName="Add Mechanic"/>
-            <div className="h-9 bg-side-nav-bg border-b-2"/>
+  return (
+    <div>
+      <ShopHeader pageName="Add Mechanic"/>
+      <div className="h-9 bg-side-nav-bg border-b-2"/>
 
-            <ToastContainer position='bottom-right' hideProgressBar={false} closeOnClick theme="light"/>
+      <ToastContainer position='bottom-right' hideProgressBar={false} closeOnClick theme="light"/>
 
             
-            <div className="flex mt-6 justify-center gap-16">
+      <div className="flex mt-6 justify-center gap-16">
 
-                <div className="flex items-center justify-center w-5/12">
-                    <img src={worker} className="w-10/12"/>
-                </div>
-                <div className="w-5/12">
-                  
-                <div className="mt-16 card p-2">
+        <div className="flex items-center justify-center w-5/12">
+          <img src={worker} className="w-10/12"/>
+        </div>
+                
+        <div className="w-5/12">  
+          <div className="mt-16 card p-2">
                  
-                 <div className="flex items-center ml-3 my-3">
-                   <p className="basis-1/3">Employee Id : </p>
-                   <input type="text" name="employeeId" value={addMechanic.employeeId} onChange={handleMechanicAdd} className="input rounded-lg p-2 w-56" maxLength={10} placeholder="EMP-0001" required/>
-                 </div>
-
-                 <div className="flex items-center ml-3 my-3">
-                   <p className="basis-1/3">Employee Name : </p>
-                   <input type="text" name="employeeName" value={addMechanic.employeeName} onChange={handleMechanicAdd} className="input rounded-lg p-2 w-56" maxLength={30} placeholder="Name here" required/>
-                 </div>
-
-                 <div className="flex items-center ml-3 my-3">
-                   <p className="basis-1/3">Contact Number : </p>
-                   <input type="text" name="contactNumber" value={addMechanic.contactNumber} onChange={handleMechanicAdd} className="input rounded-lg p-2 w-56" maxLength={10} placeholder="07 _ _ _ _ _ _ _ _" required/>
-                 </div>
-
-                 <div className="flex items-center ml-3 my-3">
-                   <p className="basis-1/3">Living Area : </p>
-                   <input type="text" name="livingArea" value={addMechanic.livingArea} onChange={handleMechanicAdd} className="input rounded-lg p-2 w-56" maxLength={40} placeholder="Village name" required/>
-                 </div>
-
-                 <div className="flex items-center ml-3 my-3">
-                   <p className="basis-1/3">Join Date : </p>
-                   <input type="date" name="joinDate" value={addMechanic.joinDate} onChange={handleMechanicAdd} className="input rounded-lg p-2 w-56" required/>
-                 </div>
-
-                 <div className="flex items-center ml-3 my-3">
-                   <p className="basis-1/3">Main specialist area : </p>
-                   <Select className="w-56"
-                    options={MainOptions}
-                    isClearable
-                    onChange={handleMechanicMainArea}
-                    styles={customStyles}
-                    placeholder="Enter main area"
-                   />
-                 </div>
-
-                 <div className="flex items-center ml-3 my-3">
-                   <p className="basis-1/3">Sub specialist area : </p>
-                   <Select className="w-56"
-                    options={MainOptions}
-                    isClearable
-                    onChange={handleMechanicSubArea}
-                    styles={customStyles}
-                    placeholder="Enter sub area"
-                   />
-                 </div>
-
-                 
-                 <div className="flex justify-end my-3 mr-10 gap-8">
-                   <button className="btn btn-warning" onClick={ ()=> setOpenCancelModal(true)}>Leave</button>
-                   <button className="btn btn-normal px-6" onClick={handleAddMechanicSubmit}>Save</button>
-                 </div>
-
-                </div>
-
-                </div>
+            <div className="flex items-center ml-3 my-3">
+              <p className="basis-1/3">Employee Id : </p>
+              <input type="text" name="employeeId" value={addMechanic.employeeId} onChange={handleMechanicAdd} 
+              className="input rounded-lg p-2 w-56" maxLength={10} placeholder="EID-0001" required/>
             </div>
 
+            <div className="flex items-center ml-3 my-3">
+              <p className="basis-1/3">Employee Name : </p>
+              <input type="text" name="employeeName" value={addMechanic.employeeName} onChange={handleMechanicAdd} 
+              className="input rounded-lg p-2 w-56" maxLength={30} placeholder="Name here" required/>
+            </div>
 
-          {/**delete modal - start */}
-          <Modal open={openCancelModal}>
-          <div onClick={(e) => e.stopPropagation()}>
+            <div className="flex items-center ml-3 my-3">
+              <p className="basis-1/3">Contact Number : </p>
+              <input type="text" name="contactNumber" value={addMechanic.contactNumber} onChange={handleMechanicAdd} 
+              className="input rounded-lg p-2 w-56" maxLength={10} placeholder="07 _ _ _ _ _ _ _ _" required/>
+            </div>
+
+            <div className="flex items-center ml-3 my-3">
+              <p className="basis-1/3">Living Area : </p>
+              <input type="text" name="livingArea" value={addMechanic.livingArea} onChange={handleMechanicAdd} 
+              className="input rounded-lg p-2 w-56" maxLength={40} placeholder="Village name" required/>
+            </div>
+
+            <div className="flex items-center ml-3 my-3">
+              <p className="basis-1/3">Join Date : </p>
+              <input type="date" name="joinDate" value={addMechanic.joinDate} onChange={handleMechanicAdd} 
+              className="input rounded-lg p-2 w-56" required/>
+            </div>
+
+            <div className="flex items-center ml-3 my-3">
+              <p className="basis-1/3">Main specialist area : </p>
+              <Select className="w-56"
+                options={MainOptions}
+                isClearable
+                onChange={handleMechanicMainArea}
+                styles={customStyles}
+                placeholder="Enter main area"
+              />
+            </div>
+
+            <div className="flex items-center ml-3 my-3">
+              <p className="basis-1/3">Sub specialist area : </p>
+              <Select className="w-56"
+                options={MainOptions}
+                isClearable
+                onChange={handleMechanicSubArea}
+                styles={customStyles}
+                placeholder="Enter sub area"
+              />
+            </div>
+
+            <div className="flex justify-end my-3 mr-10 gap-8">
+              <button className="btn btn-warning" onClick={ ()=> setOpenCancelModal(true)}>Leave</button>
+              <button className="btn btn-normal px-6" onClick={handleAddMechanicSubmit}>Save</button>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+      {/**delete modal - start */}
+      <Modal open={openCancelModal}>
+        <div onClick={(e) => e.stopPropagation()}>
          <p className="font-bold pb-2 text-red-600 text-2xl px-20 text-center mb-3">Warning!</p>
          <p className="text-text-primary text-center font-semibold px-10 ">Are you sure you want to leave?</p>
 
-
-
-
-          <div className="flex justify-center gap-8 mt-8">
+         <div className="flex justify-center gap-8 mt-8">
            <Link to="/shop/mechanics">
-           <button className="btn btn-warning">Yes</button>
+             <button className="btn btn-warning">Yes</button>
            </Link>
            <button className='btn btn-normal px-5' onClick={ ()=> setOpenCancelModal(false)}>No</button>
          </div>
        </div>   
-     </Modal>
-     {/**delete modal - end   */}
-
-
-        </div>
-    );
+      </Modal>
+      {/**delete modal - end   */}
+    </div>
+  );
 };
 
 export default MechanicsAdd;
